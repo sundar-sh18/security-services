c264a9c2998316f27d69a7613960f5185a9343b3
maaxone098 <sundar.sh18@gmail.com>
2025-08-31 14:03:27 +0530

    nav animation

--- src/app/components/nav/nav.js
+++ src/app/components/nav/nav.js
@@ -1,19 +1,31 @@
+'use client'
 import Link from 'next/link';
 import './nav.css'
+import * as motion from 'motion/react-client'
+import { useState } from 'react';
 
 export default function Nav(){
 
+    const[active, setActive] = useState(true)
 
 
     return(
         <>
             <nav>
-                <div className="logo">Alert Security Services & Labour Contract</div>
+                <motion.div initial={{ opacity: 0, transform: "translateX(-50px)"}} animate={{opacity:1,  transform: "translateX(0)"}} transition={{duration: 1}} className="logo"><Link href="/"><img src="/logo1.svg" alt="Logo" style={{width: "150px"}}/></Link></motion.div>
                 <div className="routes">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/services">Services</Link></li>
-                    <li><Link href="/about">About</Link></li>
-                    <li><Link href="/contact">Contact</Link></li>
+                    <li onClick={() => setActive("/")} style={{ color: active === "/" ? "var(--secondary)" : "black" }}>
+                        <Link href="/" >Home</Link>
+                    </li>
+                    <li onClick={() => setActive("/services")}  style={{ color: active === "/services" ? "var(--secondary)" : "black" }}>
+                        <Link href="/services">Services</Link>
+                    </li>
+                    <li onClick={() => setActive("/about")} style={{ color: active === "/about" ? "var(--secondary)" : "black" }}>
+                        <Link href="/about">About</Link>
+                    </li>
+                    <li onClick={() => setActive("/contact")}   style={{ color: active === "/contact" ? "var(--secondary)" : "black" }}>
+                        <Link href="/contact">Contact</Link>
+                    </li>
                 </div>
                 <div className="quote">
                     <Link href="/pricing">get Quote</Link>
