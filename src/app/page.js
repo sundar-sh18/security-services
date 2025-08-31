3defdfa0c1d0212f1dcff2fc24fb0bd98d41db59
maaxone098 <sundar.sh18@gmail.com>
2025-08-31 14:05:25 +0530

    page update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
 
+"use client"
 import Main from "./components/main/main";
+import "./globals.css";
+
+
 
 export default function Home() {
   return (
     <>
-    <Main/>
+  
+      
+      <Main/>
+      
+    
+    
     </>
   );
 }
