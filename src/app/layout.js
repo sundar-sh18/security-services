ff8baf4591914d0223a0d08e31c8675db6b899f3
maaxone098 <sundar.sh18@gmail.com>
2025-08-31 14:00:59 +0530

    gFonts and footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/nav/nav";
+import Footer from "./components/footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,11 +21,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/> */}
+      <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Comfortaa:wght@300..700&family=Genos:ital,wght@0,100..900;1,100..900&family=MuseoModerno:ital,wght@0,100..900;1,100..900&family=Pathway+Gothic+One&family=Rubik+Mono+One&family=Squada+One&family=Staatliches&family=Sulphur+Point:wght@300;400;700&family=Teko:wght@300..700&family=Tilt+Neon&family=Tilt+Warp&display=swap" rel="stylesheet"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Nav/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
