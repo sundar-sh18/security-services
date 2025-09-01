6bd29b7ac361f45bd4eb31f35375917a917f2136
maaxone098 <sundar.sh18@gmail.com>
2025-09-01 17:45:29 +0530

    about page added

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import About from "../components/aboutPage/about";
+import Services from "../components/servicePage/services";
+
+
+
+
+export default function Home() {
+  return (
+    <>
+      <About/>
+    </>
+  );
+}
